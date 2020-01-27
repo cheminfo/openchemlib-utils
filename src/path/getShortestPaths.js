40b079ce48131ba26e1bfb410039379ac7b30a83
Luc Patiny <luc@patiny.com>
2020-01-27 08:41:44 +0100

    chore: remove node-dijkstra

--- src/path/getShortestPaths.js
+++ src/path/getShortestPaths.js
@@ -2,45 +2,50 @@ import OCL from 'openchemlib';
 
 import { initOCL } from '../OCL';
 
-const Graph = require('node-dijkstra');
-
 initOCL(OCL);
 
 /**
  * Get the shortest path between each pair of atoms in the molecule
  * @param {OCL.Molecule} molecule
- * @returns {Array} A matrix containing on each cell (i,j) the shortest path from atom i to atom j
+ * @param {object} [options={}]
+ * @param {string} [opions.fromLabel='H']
+ * @param {string} [opions.toLabel='H']
+ * @param {string} [opions.minLenght=1]
+ * @param {string} [opions.maxLength=4]
+ * @returns {Array<Array>} A matrix containing on each cell (i,j) the shortest path from atom i to atom j
  */
-export function getShortestPaths(molecule) {
-  molecule.ensureHelperArrays(OCL.Molecule.cHelperNeighbours);
+export function getShortestPaths(molecule, options = {}) {
+  const { fromLabel = '', toLabel = '', maxLength = 3 } = options;
 
-  let nbAtoms = molecule.getAllAtoms();
-  let graph = new Map();
-  for (let i = 0; i < nbAtoms; i++) {
-    graph.set(i, new Map());
-    let l = molecule.getAllConnAtoms(i);
-    for (let j = 0; j < l; j++) {
-      graph.get(i).set(molecule.getConnAtom(i, j), 1);
-    }
-  }
+  let fromAtomicNumber = OCL.Molecule.getAtomicNoFromLabel(fromLabel);
+  let toAtomicNumber = OCL.Molecule.getAtomicNoFromLabel(toLabel);
 
-  const route = new Graph(graph);
-
+  const nbAtoms = molecule.getAllAtoms();
   let allShortestPaths = new Array(nbAtoms);
   for (let i = 0; i < nbAtoms; i++) {
     allShortestPaths[i] = new Array(nbAtoms);
   }
 
-  for (let i = 0; i < nbAtoms; i++) {
-    allShortestPaths[i][i] = [i];
-    for (let j = i + 1; j < nbAtoms; j++) {
-      let path = route.path(i, j);
-      if (path) {
-        allShortestPaths[i][j] = path.slice();
-        allShortestPaths[j][i] = path.reverse();
+  for (let from = 0; from < nbAtoms; from++) {
+    allShortestPaths[from][from] = [from];
+    for (let to = from + 1; to < nbAtoms; to++) {
+      if (
+        (fromAtomicNumber === 0 ||
+          molecule.getAtomicNo(from) === fromAtomicNumber) &&
+        (toAtomicNumber === 0 || molecule.getAtomicNo(to) === toAtomicNumber)
+      ) {
+        let path = [];
+        molecule.getPath(path, from, to, maxLength);
+        if (path.length) {
+          allShortestPaths[from][to] = path.slice();
+          allShortestPaths[to][from] = path.reverse();
+        } else {
+          allShortestPaths[from][to] = null;
+          allShortestPaths[to][from] = null;
+        }
       } else {
-        allShortestPaths[i][j] = null;
-        allShortestPaths[j][i] = null;
+        allShortestPaths[from][to] = null;
+        allShortestPaths[to][from] = null;
       }
     }
   }
