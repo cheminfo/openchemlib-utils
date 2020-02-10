4728c0ba2e3b40aac3ca7334809cb62d54b37c51
Luc Patiny <luc@patiny.com>
2020-02-10 11:24:02 +0100

    feat: add getMF

---
+++ src/util/__tests__/getMF.test.js
@@ -0,0 +1,87 @@
+import fs from 'fs';
+import { join } from 'path';
+import OCL from 'openchemlib';
+import { getMF } from '../getMF';
+
+describe('getMF', () => {
+  it('check benzene', () => {
+    let molecule = OCL.Molecule.fromSmiles('c1ccccc1');
+    let result = getMF(molecule);
+    expect(result.mf).toBe('C6H6');
+    expect(result.parts).toHaveLength(1);
+    expect(result.parts[0]).toBe('C6H6');
+  });
+  it('check glycine', () => {
+    let molecule = OCL.Molecule.fromSmiles('[NH3+]CC(=O)[O-]');
+    let result = getMF(molecule);
+    expect(result.mf).toBe('C2H5NO2');
+    expect(result.parts).toHaveLength(1);
+    expect(result.parts[0]).toBe('C2H5NO2');
+  });
+  it('check isotope of pentane', () => {
+    let molecule = OCL.Molecule.fromSmiles('CC[13CH2]CC([2H])([2H])([2H])');
+    let result = getMF(molecule);
+    expect(result.mf).toBe('C4H9[13C][2H]3');
+    expect(result.parts).toHaveLength(1);
+    expect(result.parts[0]).toBe('C4H9[13C][2H]3');
+  });
+  it('check multipart', () => {
+    let molecule = OCL.Molecule.fromSmiles('OCC(N)CCl.[CH2+][2H]');
+    let result = getMF(molecule);
+    expect(result.mf).toBe('C4H10ClNO[2H](+)');
+    expect(result.parts).toHaveLength(2);
+    expect(result.parts[0]).toBe('C3H8ClNO');
+    expect(result.parts[1]).toBe('CH2[2H](+)');
+  });
+  it('check multihydrate', () => {
+    let molecule = OCL.Molecule.fromSmiles('[ClH].O.O.O.O');
+
+    let result = getMF(molecule);
+    expect(result.mf).toBe('H9ClO4');
+    expect(result.parts).toHaveLength(2);
+    expect(result.parts[0]).toBe('4H2O');
+    expect(result.parts[1]).toBe('HCl');
+  });
+
+  it('check 4 H2O', () => {
+    let molecule = OCL.Molecule.fromSmiles('O.O.O.O');
+
+    let result = getMF(molecule);
+    expect(result.mf).toBe('H8O4');
+    expect(result.parts).toHaveLength(1);
+    expect(result.parts[0]).toBe('4H2O');
+  });
+
+  it('check Li+ OH-', () => {
+    let molecule = OCL.Molecule.fromIDCode('eDJRpCjP@');
+    let result = getMF(molecule);
+    expect(result.mf).toBe('HLiO');
+    expect(result.parts).toHaveLength(2);
+    expect(result.parts[0]).toBe('HO(-)');
+    expect(result.parts[1]).toBe('Li(+)');
+  });
+
+  it('check 2 atoms of cobalt', () => {
+    // if we have the same molecular formula we group them and count in front
+    let molecule = OCL.Molecule.fromIDCode('eDACXm`@@');
+    let result = getMF(molecule);
+    expect(result.mf).toBe('Co2');
+    expect(result.parts).toHaveLength(1);
+    expect(result.parts[0]).toBe('2Co');
+  });
+
+  it('check O--', () => {
+    let molecule = OCL.Molecule.fromSmiles('[O--]');
+    let result = getMF(molecule);
+    expect(result.mf).toBe('O(-2)');
+    expect(result.parts).toHaveLength(1);
+    expect(result.parts[0]).toBe('O(-2)');
+  });
+
+  it('multipart molfile', () => {
+    let molfile = fs.readFileSync(join(__dirname, 'ru.mol'), 'utf8');
+    let molecule = OCL.Molecule.fromMolfile(molfile);
+    let mf = getMF(molecule);
+    expect(mf).toStrictEqual({ parts: ['2C8H16', '2HORu'], mf: 'C16H34O2Ru2' });
+  });
+});
---
+++ src/util/getMF.js
@@ -0,0 +1,86 @@
+import atomSorter from 'atom-sorter';
+
+/**
+ * Calculate the molecular formula in 'chemcalc' notation taking into account fragments, isotopes and charges
+ * {OCL.Molecule} [molecule] an instance of OCL.Molecule
+ * @returns {String}
+ */
+
+export function getMF(molecule) {
+  let entries = molecule.getFragments();
+  let result = {};
+  let parts = [];
+  let allAtoms = [];
+
+  entries.forEach(function(entry) {
+    let mf = getFragmentMF(entry, allAtoms);
+    parts.push(mf);
+  });
+
+  let counts = {};
+  for (let part of parts) {
+    if (!counts[part]) counts[part] = 0;
+    counts[part]++;
+  }
+  parts = [];
+  for (let key of Object.keys(counts).sort()) {
+    if (counts[key] > 1) {
+      parts.push(counts[key] + key);
+    } else {
+      parts.push(key);
+    }
+  }
+
+  result.parts = parts;
+  result.mf = toMFString(allAtoms);
+  return result;
+}
+
+function getFragmentMF(molecule, allAtoms) {
+  let atoms = [];
+  for (let i = 0; i < molecule.getAllAtoms(); i++) {
+    let atom = {};
+    atom.charge = molecule.getAtomCharge(i);
+    atom.label = molecule.getAtomLabel(i);
+    atom.mass = molecule.getAtomMass(i);
+    atom.implicitHydrogens = molecule.getImplicitHydrogens(i);
+    atoms.push(atom);
+    allAtoms.push(atom);
+  }
+  return toMFString(atoms);
+}
+
+function toMFString(atoms) {
+  let charge = 0;
+  let mfs = {};
+  for (let atom of atoms) {
+    let label = atom.label;
+    charge += atom.charge;
+    if (atom.mass) {
+      label = `[${atom.mass}${label}]`;
+    }
+    let mfAtom = mfs[label];
+    if (!mfAtom) {
+      mfs[label] = 0;
+    }
+    mfs[label] += 1;
+    if (atom.implicitHydrogens) {
+      if (!mfs.H) mfs.H = 0;
+      mfs.H += atom.implicitHydrogens;
+    }
+  }
+
+  let mf = '';
+  let keys = Object.keys(mfs).sort(atomSorter);
+  for (let key of keys) {
+    mf += key;
+    if (mfs[key] > 1) mf += mfs[key];
+  }
+
+  if (charge > 0) {
+    mf += `(+${charge > 1 ? charge : ''})`;
+  } else if (charge < 0) {
+    mf += `(${charge < -1 ? charge : '-'})`;
+  }
+  return mf;
+}
