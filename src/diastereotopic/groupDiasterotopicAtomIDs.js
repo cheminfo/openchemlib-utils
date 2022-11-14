9d4304c4c6be982cbfbc84590e83132e9054dec5
jobo322 <jose.bolanos@correounivalle.edu.co>
2022-11-14 07:11:41 -0500

    fix: split groupedDiasterotopicAtomIDs

---
+++ src/diastereotopic/groupDiasterotopicAtomIDs.js
@@ -0,0 +1,21 @@
+export function groupDiastereotopicAtomIDs(diaIDs, molecule, options) {
+  const { atomLabel } = options;
+  const diaIDsObject = {};
+  for (let i = 0; i < diaIDs.length; i++) {
+    if (!atomLabel || molecule.getAtomLabel(i) === atomLabel) {
+      let diaID = diaIDs[i];
+      if (!diaIDsObject[diaID]) {
+        diaIDsObject[diaID] = {
+          counter: 0,
+          atoms: [],
+          oclID: diaID,
+          atomLabel: molecule.getAtomLabel(i),
+        };
+      }
+      diaIDsObject[diaID].counter++;
+      diaIDsObject[diaID].atoms.push(i);
+    }
+  }
+
+  return Object.keys(diaIDsObject).map((key) => diaIDsObject[key]);
+}
