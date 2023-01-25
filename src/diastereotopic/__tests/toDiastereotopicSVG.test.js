74607c218b695b8ce7d382e2777a61548ad902a8
Luc Patiny <luc@patiny.com>
2023-01-25 10:17:18 +0100

    chore: fix testcase for new SVG version (font-family css)

--- src/diastereotopic/__tests/toDiastereotopicSVG.test.js
+++ src/diastereotopic/__tests/toDiastereotopicSVG.test.js
@@ -6,7 +6,7 @@ describe('toDiastereotopicSVG', () => {
   it('CCC', () => {
     let molecule = OCL.Molecule.fromSmiles('CCC');
     let svg = toDiastereotopicSVG(molecule);
-    expect(svg).toHaveLength(996);
     expect(svg).toMatchSnapshot();
+    expect(svg).toHaveLength(1028);
   });
 });
