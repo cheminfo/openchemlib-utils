579b10ab6ab0697e66eac5ede4732dc4fc4443fe
Luc Patiny <luc@patiny.com>
2023-01-25 13:57:22 +0100

    chore: fix utility to convert Dwar files

--- rxn/convertDwar.mjs
+++ rxn/convertDwar.mjs
@@ -1,9 +1,12 @@
 // execute this file with `node rxn/convertDwar.mjs`
+// but first you will have to `npm run prepack`
 
 import { writeFileSync, readFileSync } from 'fs';
 
+import { parseDwar } from '../lib/index.js';
+
 const dwar = readFileSync(new URL('reactions.dwar', import.meta.url), 'utf8');
-const reactions = convertDwar(dwar);
+const reactions = parseDwar(dwar).data;
 
 writeFileSync(
   new URL('../src/reaction/reactionsDatabase.js', import.meta.url),
@@ -15,35 +18,9 @@ const dwarTest = readFileSync(
   new URL('reactions.test.dwar', import.meta.url),
   'utf8',
 );
-const reactionsTest = convertDwar(dwarTest);
+const reactionsTest = parseDwar(dwarTest).data;
 writeFileSync(
   new URL('../src/reaction/__tests__/reactionsDatabase.js', import.meta.url),
   `export const reactionsDatabase=${JSON.stringify(reactionsTest)}`,
   'utf8',
 );
-
-function convertDwar(dwar) {
-  let lines = dwar
-    .split(/\r?\n/)
-    .filter((line) => !line.match(/^<.*>$/))
-    .filter((line) => !line.match(/^[\t ]*$/));
-  const reactions = [];
-  const headers = lines.shift().split('\t');
-  if (headers.filter((header) => header === 'rxnCode').length === 0) {
-    throw new Error('Missing column named rxnCode');
-  }
-
-  for (let line of lines) {
-    const fields = line.split('\t');
-    const reaction = {};
-    reaction.rxnCode = `${fields[headers.indexOf('rxnCode')]}#${
-      fields[headers.indexOf('atomMapping')]
-    }#${fields[headers.indexOf('idcoordinates2D')]}`;
-    headers.forEach((header, index) => {
-      if (header.match(/^[a-z]/)) return;
-      reaction[header] = fields[index];
-    });
-    reactions.push(reaction);
-  }
-  return reactions;
-}
