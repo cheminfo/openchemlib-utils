a7f0fb5b7951995a697a363a95a08bf746c3c6d1
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-07-25 15:59:21 +0200

    feat: get em and charge for products

--- src/reaction/__tests__/reactionsDatabase.js
+++ src/reaction/__tests__/reactionsDatabase.js
@@ -6,16 +6,30 @@ export const reactionsDatabase = [
   {
     Label: 'SH to SMe',
     rxnCode: 'eFB@HcAa@!eMB@HchH#S\\ S\\@#!R@FL]bgp !R@FL?XqwZ_@',
-    needToBeCharged: false,
   },
   {
     Label: 'SH to OH',
     rxnCode: 'eFB@HcA}E`!eFHBLGtV#aP aP#!R@FL]Rgp !R@FL]vgp',
-    needToBeCharged: false,
   },
   {
-    Label: 'Charged reaction',
-    rxnCode: 'eFHBLGtV!eFB@HcA}E`#aP aP#!R@AL]\\mp !R@AL]Nmp',
-    needToBeCharged: true,
+    Label: 'Ionization',
+    rxnCode: 'eFHBLGtP!eFH`fLGtX#Qd Qd#!R@FL]Rgp !R@FL]vgp',
+  },
+  {
+    Label: 'Ionization',
+    rxnCode: 'eMHBN``!eMH`fN`~b@#Qg Qg#!R@FL?XqtJ_@ !R@FL?XqwZ_@',
+  },
+  {
+    Label: 'Ionization',
+    rxnCode: 'eMHBN``!gCaIHMBRduPD#Qg QIp#!R@FL?XqvJ_@ !R_g?p?[_}G]k',
+  },
+  {
+    Label: 'Ionization',
+    rxnCode: 'eMHBN``!gCaIHLbRduPD#Qg QIp#!R@FL?XqvJ_@ !R_g?p?[_}G]k',
+  },
+  {
+    Label: 'Ionization',
+    rxnCode:
+      'eMHBN``!dazHbBCH@IrRERUUPANL@#Q^ Q^@@#!R@AM?Dqv|_@ !RbOvw?_x@b@JH__x@mpH]^gp',
   },
 ];
