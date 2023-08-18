5d859e041fc168494c83e3b04278f6b540efd737
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-08-18 13:15:44 +0200

    fix: missing docs and undo digits rounding

--- src/reaction/utils/getReactantInfo.js
+++ src/reaction/utils/getReactantInfo.js
@@ -2,23 +2,24 @@ import { getCharge } from '../../util/getCharge.js';
 import { getMF } from '../../util/getMF.js';
 
 /**
- *
+ * @description Get the info of a molecule
  * @param {import('openchemlib').Molecule} molecule
- * @param {Map} moleculesInfo
- * @returns
+ * @param {Map} moleculesInfo Map of molecules info
+ * @returns {Object} object with molfile, idCode, mf, em, mz and charge
  */
 export function getInfo(molecule, moleculesInfo) {
   if (moleculesInfo.has(molecule)) {
     return moleculesInfo.get(molecule);
   }
-  let exactMass = molecule.getMolecularFormula().absoluteWeight;
+  let em = molecule.getMolecularFormula().absoluteWeight;
   let charge = getCharge(molecule);
+  let mz = em / (charge === 0 ? 1 : Math.abs(charge));
   const reactantInfo = {
     molfile: molecule.toMolfile(),
     idCode: molecule.getIDCode(),
     mf: getMF(molecule).mf,
-    em: exactMass,
-    mz: exactMass / (charge === 0 ? 1 : Math.abs(charge)),
+    em,
+    mz,
     charge,
   };
   moleculesInfo.set(molecule, reactantInfo);
