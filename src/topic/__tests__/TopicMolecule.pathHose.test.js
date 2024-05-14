f16250abee578a82e259abe5eb2cd6215d7ecd2e
Luc Patiny <luc@patiny.com>
2024-05-14 08:25:47 +0200

    chore: fix import and '.js'

--- src/topic/__tests__/TopicMolecule.pathHose.test.js
+++ src/topic/__tests__/TopicMolecule.pathHose.test.js
@@ -1,21 +1,20 @@
-import { Molecule } from "openchemlib";
-import { test, expect } from "vitest";
+import { Molecule } from 'openchemlib';
+import { test, expect } from 'vitest';
 
-import { makeRacemic } from "../../util/makeRacemic.js";
-import { TopicMolecule } from "../TopicMolecule.js";
+import { makeRacemic } from '../../util/makeRacemic';
+import { TopicMolecule } from '../TopicMolecule';
 
-
-test("TopicMolecule.path", () => {
-
+test('TopicMolecule.path', () => {
   const molecule = Molecule.fromSmiles('CCCCCO');
   const topicMolecule = new TopicMolecule(molecule);
 
-  expect(topicMolecule.getAtomPaths(0, 2, { distance: 2 })).toStrictEqual([[0, 1, 2]])
-  expect(topicMolecule.getAtomPaths(0, 2, { distance: 3 })).toStrictEqual([])
+  expect(topicMolecule.getAtomPaths(0, 2, { distance: 2 })).toStrictEqual([
+    [0, 1, 2],
+  ]);
+  expect(topicMolecule.getAtomPaths(0, 2, { distance: 3 })).toStrictEqual([]);
+});
 
-})
-
-test("TopicMolecule.getHoseFragment", async () => {
+test('TopicMolecule.getHoseFragment', async () => {
   const molecule = Molecule.fromSmiles('Cl/C=C/CCCCl');
   //const molecule = Molecule.fromSmiles('C1=CC=CC=C1');
   molecule.addImplicitHydrogens();
@@ -23,18 +22,22 @@ test("TopicMolecule.getHoseFragment", async () => {
   const topicMolecule = new TopicMolecule(molecule);
 
   const fragment = topicMolecule.getHoseFragment([0, 1, 2, 9], {
-    sphereSize: 1, tagAtoms: [2, 9],
+    sphereSize: 1,
+    tagAtoms: [2, 9],
     tagAtomFct: (fragment, atom) => {
-      fragment.setAtomMass(atom, 255)
-    }
-  })
+      fragment.setAtomMass(atom, 255);
+    },
+  });
 
   makeRacemic(fragment);
 
-
-
-  expect(fragment.toIsomericSmiles().replaceAll('255', '*')).toBe("[*H]C/[*C]=C/Cl")
-  expect(fragment.toIsomericSmiles({ createSmarts: true }).replaceAll('255', '*')).toBe('[*H]C/[*C;!H0]=[C;!H0]/Cl')
-  expect(fragment.toIsomericSmiles({ kekulizedOutput: true }).replaceAll('255', '*')).toBe('[*H]C/[*C]=C/Cl')
-
-})
+  expect(fragment.toIsomericSmiles().replaceAll('255', '*')).toBe(
+    '[*H]C/[*C]=C/Cl',
+  );
+  expect(
+    fragment.toIsomericSmiles({ createSmarts: true }).replaceAll('255', '*'),
+  ).toBe('[*H]C/[*C;!H0]=[C;!H0]/Cl');
+  expect(
+    fragment.toIsomericSmiles({ kekulizedOutput: true }).replaceAll('255', '*'),
+  ).toBe('[*H]C/[*C]=C/Cl');
+});
