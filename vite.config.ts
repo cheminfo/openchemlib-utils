a111338064d07764b173717e1210a66dd846a2a1
Michaël Zasso <targos@protonmail.com>
2025-02-06 16:32:13 +0100

    fix: update OCL

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    // Tests are generally slow because they have to load OCL each time.
+    testTimeout: 15_000,
     include: ['./src/**/*.test.js', './src/**/*.test.ts'],
   },
 });
