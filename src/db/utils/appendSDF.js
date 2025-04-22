3af4bc0e09c086cee698af2207f0e6ff51e1f2a6
Luc Patiny <luc@patiny.com>
2025-04-22 21:19:14 +0200

    feat: add option mixedEOL in appendSDF

--- src/db/utils/appendSDF.js
+++ src/db/utils/appendSDF.js
@@ -2,12 +2,13 @@ import { ensureString } from 'ensure-string';
 import { parse } from 'sdf-parser';
 
 export default async function appendSDF(moleculesDB, sdf, options = {}) {
-  const { onStep } = options;
+  const { onStep, dynamicTyping, mixedEOL, eol } = options;
+
   sdf = ensureString(sdf);
   if (typeof sdf !== 'string') {
     throw new TypeError('sdf must be a string');
   }
-  const parsed = parse(sdf);
+  const parsed = parse(sdf, { dynamicTyping, mixedEOL, eol });
   moleculesDB.statistics = parsed.statistics;
   for (let i = 0; i < parsed.molecules.length; i++) {
     const molecule = parsed.molecules[i];
