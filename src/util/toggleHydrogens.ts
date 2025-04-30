8d81b90c757d441c827fd4ff4a6c52bbee8b8564
Luc Patiny <luc@patiny.com>
2025-04-30 10:49:56 +0200

    chore: toggleHydrogens use native getImplicitHydrogens

--- src/util/toggleHydrogens.ts
+++ src/util/toggleHydrogens.ts
@@ -1,14 +1,12 @@
 import type { Molecule } from 'openchemlib';
 
-import { getImplicitHydrogensCount } from './getImplicitHydrogensCount.js';
-
 /**
  * Toggle presence of implicity hydrogens on/off
  * @param molecule
  * @param atomID
  */
 export function toggleHydrogens(molecule: Molecule, atomID: number) {
-  if (getImplicitHydrogensCount(molecule, atomID) === 0) {
+  if (molecule.getImplicitHydrogens(atomID) === 0) {
     const atomsToDelete = [];
     for (let i = 0; i < molecule.getAllConnAtoms(atomID); i++) {
       const connectedAtom = molecule.getConnAtom(atomID, i);
