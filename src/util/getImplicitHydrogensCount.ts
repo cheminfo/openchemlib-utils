77e62c316cca6c1173b0eaf5b8e501e4199066a7
Luc Patiny <luc@patiny.com>
2025-04-30 10:48:16 +0200

    chore: getImplicitHydrogensCount use the native method.

--- src/util/getImplicitHydrogensCount.ts
+++ src/util/getImplicitHydrogensCount.ts
@@ -1,10 +1,5 @@
 import type { Molecule } from 'openchemlib';
 
 export function getImplicitHydrogensCount(molecule: Molecule, atomID: number) {
-  molecule.ensureHelperArrays(molecule.getOCL().Molecule.cHelperNeighbours);
-  return (
-    molecule.getAllHydrogens(atomID) +
-    molecule.getConnAtoms(atomID) -
-    molecule.getAllConnAtoms(atomID)
-  );
+  return molecule.getImplicitHydrogens(atomID);
 }
