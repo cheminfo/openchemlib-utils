64db46d6dc6d042afb7ed2187f7933ffb9c4f308
Luc Patiny <luc@patiny.com>
2025-08-22 16:18:40 +0200

    docs: improve jsdoc

--- src/hose/getHoseCodesForAtomsInternal.js
+++ src/hose/getHoseCodesForAtomsInternal.js
@@ -7,13 +7,18 @@ export const FULL_HOSE_CODE = 1;
 export const HOSE_CODE_CUT_C_SP3_SP3 = 2;
 
 /**
- * Returns the hose code for specific atom numbers
- * @param {import('openchemlib').Molecule} molecule - The OCL molecule with expandedImplicitHydrogens and ensureHeterotopicChiralBonds
- * @param {object} [options={}]
+ * Returns an array of strings (idCodes) specified molecule. Each string corresponds to a
+ * hose code. By default it will calculate the hose codes for sphere 0 to 4 and will reuse
+ * the existing tagged atoms.
+ * @param {import('openchemlib').Molecule} molecule - The OCL molecule to process.
+ * @param {object} options - Options for generating hose codes.
  * @param {string[]} [options.allowedCustomLabels] - Array of the custom labels that are considered as root atoms. By default all atoms having a customLabel
  * @param {number} [options.minSphereSize=0] - Smallest hose code sphere
  * @param {number} [options.maxSphereSize=4] - Largest hose code sphere
  * @param {number} [options.kind=FULL_HOSE_CODE] - Kind of hose code, default usual sphere
+ * @param {number[]} [options.tagAtoms=[]] - Array of atom indices to tag as root atoms
+ * @param {Function} [options.tagAtomFct=tagAtom] - Function to tag an atom as root atom. By default it is defined internal
+ * @returns {Array} - An array of hose code fragments.
  */
 export function getHoseCodesForAtomsInternal(molecule, options = {}) {
   const fragments = getHoseCodesForAtomsAsFragments(molecule, options);
@@ -29,6 +34,18 @@ export function getHoseCodesForAtomsInternal(molecule, options = {}) {
   return hoses;
 }
 
+/**
+ * Returns an array of hose code fragments for the specified molecule.
+ * @param {import('openchemlib').Molecule} molecule - The OCL molecule to process.
+ * @param {object} options - Options for generating hose codes.
+ * @param {string[]} [options.allowedCustomLabels] - Array of the custom labels that are considered as root atoms. By default all atoms having a customLabel
+ * @param {number} [options.minSphereSize=0] - Smallest hose code sphere
+ * @param {number} [options.maxSphereSize=4] - Largest hose code sphere
+ * @param {number} [options.kind=FULL_HOSE_CODE] - Kind of hose code, default usual sphere
+ * @param {number[]} [options.tagAtoms=[]] - Array of atom indices to tag as root atoms
+ * @param {Function} [options.tagAtomFct=tagAtom] - Function to tag an atom as root atom. By default it is defined internal
+ * @returns {Array} - An array of hose code fragments.
+ */
 export function getHoseCodesForAtomsAsFragments(molecule, options = {}) {
   const OCL = molecule.getOCL();
   const {
