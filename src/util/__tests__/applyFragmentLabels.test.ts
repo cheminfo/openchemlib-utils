60b2ccf67eca8975ce17fba28d3ebd3294b8f5a4
Luc Patiny <lpatiny@users.noreply.github.com>
2025-10-15 10:14:00 +0200

    feat: new utility function applyFragmentLabels to label atoms (#192)
    * feat: new utility function applyFragmentLabels to label atoms
    
    * feat: add applyFragmentLabels

---
+++ src/util/__tests__/applyFragmentLabels.test.ts
@@ -0,0 +1,76 @@
+import { Molecule } from 'openchemlib';
+import { expect, test } from 'vitest';
+
+import { applyFragmentLabels } from '../applyFragmentLabels.ts';
+
+test('default option find all matching', () => {
+  const molecule = Molecule.fromSmiles('CCC(=O)CCCCCCC(=O)CC');
+  const fragment = Molecule.fromSmiles('CCC(=O)');
+  fragment.setAtomCustomLabel(0, 'β');
+  fragment.setAtomCustomLabel(1, 'ɑ');
+  fragment.setFragment(true);
+
+  const found = applyFragmentLabels(molecule, fragment);
+
+  expect(found).toBe(4);
+
+  const labels = [];
+  for (let i = 0; i < molecule.getAllAtoms(); i++) {
+    labels.push(molecule.getAtomCustomLabel(i));
+  }
+
+  expect(labels).toStrictEqual([
+    'β',
+    'ɑ',
+    null,
+    null,
+    'ɑ',
+    'β',
+    null,
+    null,
+    'β',
+    'ɑ',
+    null,
+    null,
+    'ɑ',
+    'β',
+  ]);
+  expect(molecule.toMolfile()).toMatchSnapshot();
+});
+
+test('separated, we can not reuse twice the same atoms', () => {
+  const molecule = Molecule.fromSmiles('CCC(=O)CCCCCCC(=O)CC');
+  const fragment = Molecule.fromSmiles('CCC(=O)');
+  fragment.setAtomCustomLabel(0, 'β');
+  fragment.setAtomCustomLabel(1, 'ɑ');
+  fragment.setFragment(true);
+
+  const found = applyFragmentLabels(molecule, fragment, {
+    algorithm: 'separated',
+  });
+
+  expect(found).toBe(2);
+
+  const labels = [];
+  for (let i = 0; i < molecule.getAllAtoms(); i++) {
+    labels.push(molecule.getAtomCustomLabel(i));
+  }
+
+  expect(labels).toStrictEqual([
+    'β',
+    'ɑ',
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    'β',
+    'ɑ',
+    null,
+    null,
+    null,
+    null,
+  ]);
+  expect(molecule.toMolfile()).toMatchSnapshot();
+});
