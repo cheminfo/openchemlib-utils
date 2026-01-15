39504c51bf2e0a573eb3436e4ca742f6d371c059
Luc Patiny <luc@patiny.com>
2026-01-15 14:46:09 +0100

    feat: add coordinates in autoLabelDatabase

--- src/util/autoLabelDatabase.ts
+++ src/util/autoLabelDatabase.ts
@@ -2,46 +2,54 @@ export const autoLabelDatabase = [
   {
     idCode:
       'fmo@@@LddTTVbvebQbrTyjUcNBfjjjjjjjjjh@Oz[PG]cAhMv\\anulMvLSnqiuFzNYwXt[c][B[iZS]SJ{lXmin}c]vLflw\\sn{Fs^{lZlGnqnt^{FGI{lYmgni`q^zfSU{jZLwnilw^zfKC{jZmOninr~zf[@',
-    mf: 'C27',
-    mw: 324.29699999999997,
+    coordinates:
+      '!BetK~gzaBM`BWO_z`esvhOZ`}etK~gy]BjDK~hL~esyhqXNLxi]BWP`QLesvWPi\\}?i~WP`',
+    mf: 'C27H48',
+    mw: 372.67812,
     label: 'cholesterol',
   },
   {
     idCode:
       'flm@@@LddTTVbvebyjUcNBfjjjjjjj@C~fTAwXpZC]gH[m[C]cD{lZ]Qncf]vMFxwVpfzVdwTrn{FKZ[oXw]cIkMwL{nqlwn{FkA{l[mGnqar^{F[@',
-    mf: 'C19',
-    mw: 228.20899999999997,
+    coordinates: '!BetK~gzaBM`BWO_z`esvhOZ`}etK~gy]BjDK~hL~esyhqXNLx',
+    mf: 'C19H32',
+    mw: 260.46308,
     label: 'steroid',
   },
   {
     idCode:
       'fhe@@@LdbbbTReryHQmN|Fjjjjjj`@iE@TzCFCHavFMFIUjTYcFK\\npfLfeMeQ[TfLqlufMV]YoL[lEcM`',
-    mf: 'C17',
-    mw: 204.18699999999998,
+    coordinates: '!BbOvw?_x@?g~H@k\\B?g~w@k_}bOvH?_x@GvVH@a|ZTq`',
+    mf: 'C17H28',
+    mw: 232.40931999999998,
     label: 'steroid-core',
   },
   {
     idCode: 'daDD@@qJYnnjjh@hrPwXrMvlS]STwRsMums]s@',
-    mf: 'C6O2',
-    mw: 104.06400000000001,
+    coordinates: '!Bm?vH?[_|?g?~_x`BbOt',
+    mf: 'C6H12O2',
+    mw: 116.15928000000001,
     label: 'pyranose',
   },
   {
     idCode: 'gOp`AdifMjj`OzUHwXr[iXmuMV{f[]k@',
-    mf: 'C5O2',
-    mw: 92.053,
+    coordinates: '!BTv]`YW?~KF_~_q`l',
+    mf: 'C5H10O2',
+    mw: 102.1324,
     label: 'furanose',
   },
   {
     idCode: 'gOp`Adigkjj`OzUHwXr[mXmuMVzV[]s@',
-    mf: 'C5O2',
-    mw: 92.053,
+    coordinates: '!Bm?vH?[_|?g?~_xc}',
+    mf: 'C5H10O2',
+    mw: 102.1324,
     label: 'pyranose-core',
   },
   {
     idCode: 'gFp`AdidvjhC~aRMvLfzVK]SUny`',
-    mf: 'C4O2',
-    mw: 80.042,
+    coordinates: '!BTv]`YW?~KF_~_p',
+    mf: 'C4H8O2',
+    mw: 88.10552,
     label: 'furanose-core',
   },
 ];
