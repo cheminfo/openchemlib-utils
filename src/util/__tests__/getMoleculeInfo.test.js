239d08f34eb349d4a19ec2f9a9c0fc20b8209cfd
Michaël Zasso <targos@protonmail.com>
2026-08-14 10:33:10 +0200

    feat: update OCL to v9.25.0 (#245)

--- src/util/__tests__/getMoleculeInfo.test.js
+++ src/util/__tests__/getMoleculeInfo.test.js
@@ -8,26 +8,28 @@ test('getMoleculeInfo', () => {
 
   const info = getMoleculeInfo(molecule, new Map());
 
-  expect(info).toStrictEqual({
-    molfile:
-      '\n' +
-      'Actelion Java MolfileCreator 1.0\n' +
-      '\n' +
-      '  3  2  0  0  0  0  0  0  0  0999 V2000\n' +
-      '    1.7321   -0.5000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n' +
-      '    0.8660    0.0000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n' +
-      '    0.0000   -0.5000    0.0000 O   0  0  0  0  0  0  0  0  0  0  0  0\n' +
-      '  1  2  1  0  0  0  0\n' +
-      '  2  3  1  0  0  0  0\n' +
-      'M  CHG  1   1   1\n' +
-      'M  ISO  1   1  12\n' +
-      'M  RAD  1   1   3\n' +
-      'M  END\n',
-    idCode: 'eMHaeIhFILOzg@',
-    mf: 'CH3O[12C](+)',
-    mw: 43.033963083220854,
-    em: 43.01838971626,
-    mz: 43.01784113635093,
-    charge: 1,
-  });
+  expect(info).toMatchInlineSnapshot(`
+    {
+      "charge": 1,
+      "em": 43.01838971626,
+      "idCode": "eMHaeIhFILOzg@",
+      "mf": "CH3O[12C](+)",
+      "molfile": "
+    OCL MolfileCreator  2D
+
+      3  2  0  0  0  0  0  0  0  0999 V2000
+        1.7321   -0.5000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+        0.8660    0.0000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+        0.0000   -0.5000    0.0000 O   0  0  0  0  0  0  0  0  0  0  0  0
+      1  2  1  0  0  0  0
+      2  3  1  0  0  0  0
+    M  CHG  1   1   1
+    M  ISO  1   1  12
+    M  RAD  1   1   3
+    M  END
+    ",
+      "mw": 43.033963083220854,
+      "mz": 43.01784113635093,
+    }
+  `);
 });
